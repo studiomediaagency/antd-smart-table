8babd09c5576c0a880038d14f0a6b9426efa8759
Austin Woodall <austin.woodall72@gmail.com>
2020-08-28 09:37:28 -0500

    smart table entry point

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SmartTable from "./App";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SmartTable />
   </React.StrictMode>,
   document.getElementById('root')
 );
